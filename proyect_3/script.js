199f7ce019312c647db1e2f694a0dd4168c63c49
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-08-01 16:08:20 -0500

    Colisions proyect 4

--- proyect_3/script.js
+++ proyect_3/script.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20;
+const numberOfEnemies = 30;
 ;
 const enemiesArray = [];
 
@@ -13,28 +13,39 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = './enemy2.png';
+        this.image.src = './enemy4.png';
         // this.y = Math.random() * canvas.height;
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth / 2;
         this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor( Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
+        // this.angle = Math.random() * 100;
+        // this.angleSpeed = Math.random() * 0.5 + 0.5;
+        // this.curve = Math.random() * 200 + 50;
+        this.interval = Math.floor( Math.random() * 200 + 50);
     }
     update(){
-        this.x += this.speed;
-        this.y += Math.sin(this.angle);
-        this.angle += this.angleSpeed;
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/20;
+        this.y -= dy/20;
+        // this.x = 0;
+        // this.y = 0;
         if (this.x + this.width < 0) this.x = canvas.width;
         //animate sprites
         if (gameFrame % this.flapSpeed === 0){
-            this.frame > 4 ? this.frame = 0 ? this.frame = 1 : this.frame = 0 : this.frame++;
+            this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw(){
